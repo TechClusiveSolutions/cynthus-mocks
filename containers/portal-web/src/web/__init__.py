e6f33c26a2e558b00916d30da722659cc8085a30
Jad Wauthier <JWauthier@TechClusiveSolutions.com>
2026-05-31 17:49:11 -0500

    Remove references to redis

--- containers/portal-web/src/web/__init__.py
+++ containers/portal-web/src/web/__init__.py
@@ -2,39 +2,17 @@
 
 import logging
 
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from os import environ
-from redis import asyncio as aioredis
 
-from web.routes import event, system
-
-
-@asynccontextmanager
-async def lifespan(application: FastAPI):
-    host = environ.get('CACHE_HOST')
-    port = environ.get('CACHE_PORT')
-    username = environ.get('CACHE_USERNAME')
-    password = environ.get('CACHE_PASSWORD')
-    url = f'redis://{username}:{password}@{host}:{port}/0'
-    client = aioredis.from_url(url)
-
-    # Startup: Initialize Redis pool
-    application.state.redis = client
-
-    yield
-
-    # Shutdown: Close Redis pool
-    await application.state.redis.close()
+from web.routes import system
 
 
 def initialize():
     logger = logging.getLogger(__name__)
-    application = FastAPI(lifespan=lifespan)
+    application = FastAPI()
 
     logger.debug('Configuring Portal application')
 
-    application.include_router(event)
     application.include_router(system)
 
     return application
